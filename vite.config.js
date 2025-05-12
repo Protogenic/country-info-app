ad8ea058f549cb23ecdb64657fdc945080d3b9cd
Vladimir Bezrukov <82569672+Protogenic@users.noreply.github.com>
2025-05-12 15:04:04 +0300

    adgfdg

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/country-info-app/', // ← это важно для GitHub Pages
   plugins: [react()],
 })
